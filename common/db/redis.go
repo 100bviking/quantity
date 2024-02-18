b4355ec6ba78c4e5f7ecc7844fbfea9b92376008
100bviking <100bviking@gmail.com>
2024-02-19 00:27:06 +0800

    fix init redis

--- common/db/redis.go
+++ common/db/redis.go
@@ -9,12 +9,11 @@ var Redis *redis.Client
 
 func init() {
 	Redis = redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
-		Password: "", // no password set
-		DB:       0,  // use default DB
+		Addr: "127.0.0.1:6379",
+		DB:   0, // use default DB
 	})
 	err := Redis.Ping(context.Background())
-	if err != nil {
+	if err.Err() != nil {
 		panic("init redis client failed.")
 	}
 }
