9b444d399f3db3a6a374f2207f324f5865fa0ecd
100bviking <100bviking@gmail.com>
2024-02-25 16:09:11 +0800

    add view

--- clickhouse.sql
+++ clickhouse.sql
@@ -15,3 +15,26 @@ create table default.kline
         ORDER BY (symbol, k_start_time)
         SETTINGS index_granularity = 8192
         comment 'k线数据,按照1小时存储';
+
+create view day7_avg_kline as
+select symbol,
+       k_start_time,
+       avg(toFloat64OrZero(end_price))
+           over (PARTITION BY symbol ORDER BY k_start_time range between 167 preceding and current row ) as avg_price
+from kline;
+
+
+create view day25_avg_kline as
+select symbol,
+       k_start_time,
+       avg(toFloat64OrZero(end_price))
+           over (PARTITION BY symbol ORDER BY k_start_time range between 599 preceding and current row ) as avg_price
+from kline;
+
+create view day99_avg_kline as
+select symbol,
+       k_start_time,
+       avg(toFloat64OrZero(end_price))
+           over (PARTITION BY symbol ORDER BY k_start_time range between 2375 preceding and current row ) as avg_price
+from kline
+
