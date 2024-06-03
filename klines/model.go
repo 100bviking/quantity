63facc3affd9ba89e7cb0984dd711a080bfe61a7
viking <100bviking@gmail.com>
2024-06-04 02:00:39 +0800

    fix save lines

--- klines/model.go
+++ klines/model.go
@@ -7,8 +7,20 @@ import (
 
 func saveKLinesPrice(kLines []*common.KLine) (err error) {
 	kline := new(common.KLine)
-	err = db.KLinesDB.Model(kline).Create(kLines).Error
-	if err != nil {
+	for _, line := range kLines {
+		// check exist or not.
+		var cnt int64
+		err = db.KLinesDB.Model(kline).Where("symbol = ? and k_start_time = ?", line.Symbol, line.KStartTime).Count(&cnt).Error
+		if err != nil {
+			return
+		}
+
+		if cnt > 0 {
+			continue
+		}
+		err = db.KLinesDB.Model(kline).Create(line).Error
+		if err != nil {
+		}
 		return
 	}
 	return
